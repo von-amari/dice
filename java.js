08f63fc314633a8988ae3e6383fef9a2944749e4
von-amari <60331179+von-amari@users.noreply.github.com>
2020-02-24 20:18:00 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,24 @@
+
+var randomNumber1= Math.floor(Math.random()*6)+1;
+var randomNumber2= Math.floor(Math.random()*6)+1;
+
+
+var diceImage1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelector(".img1").setAttribute("src", diceImage1);
+
+var diceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img2").setAttribute("src", diceImage2);
+
+
+if (randomNumber1 === randomNumber2) {
+document.querySelector("h1").textContent="Draw";
+}
+
+else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").textContent="🚩Player 1 Wins";
+}
+else {
+  document.querySelector("h1").textContent="Player 2 Wins🚩";
+}
